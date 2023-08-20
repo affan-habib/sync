28bc7a31a359a3c084dbec57b1b5cafb2bfb04ad
affan-habib <affan.habib44@gmail.com>
2023-08-20 18:19:09 +0600

    colums updated

--- src/features/orders/OrdersColumns.tsx
+++ src/features/orders/OrdersColumns.tsx
@@ -3,49 +3,8 @@ import { ColumnDef } from "@tanstack/react-table";
 
 export const OrdersColumns: ColumnDef<any, any>[] = [
   {
-    header: "Order Number",
-    cell: (cell) => cell.row.original.id,
-    maxSize: 10,
-  },
-  {
     header: "Customer Name",
     cell: (cell) => cell.row.original.user.full_name,
-  },
-  {
-    header: "Status",
-    cell: (cell) => (
-      <Chip
-        label={cell.row.original.status.toUpperCase()}
-        variant="outlined"
-        color={
-          cell.row.original.status === "pending"
-            ? "warning"
-            : cell.row.original.status === "processing"
-            ? "info"
-            : "default"
-        }
-      />
-    ),
-  },
-  {
-    header: "Payment Method",
-    cell: (cell) => (
-      <Chip
-        label={cell.row.original.payment_method.toUpperCase()}
-        variant="filled"
-        color={
-          cell.row.original.payment_method === "wallet"
-            ? "primary"
-            : cell.row.original.payment_method === "CreditCard"
-            ? "success"
-            : cell.row.original.payment_method === "Benefit"
-            ? "info"
-            : cell.row.original.payment_method === "cash"
-            ? "error"
-            : "warning"
-        }
-      />
-    ),
   },
   {
     header: "Mobile Num",
@@ -68,6 +27,44 @@ export const OrdersColumns: ColumnDef<any, any>[] = [
       <Button
         style={{ marginLeft: "4px" }}
       >{`${cell.row.original.items.length} items`}</Button>
+    ),
+  },
+  {
+    header: "Status",
+    cell: (cell) => (
+      <Chip
+        label={cell.row.original.status.toUpperCase()}
+        size="small"
+        variant="outlined"
+        color={
+          cell.row.original.status === "pending"
+            ? "warning"
+            : cell.row.original.status === "processing"
+            ? "info"
+            : "default"
+        }
+      />
+    ),
+  },
+  {
+    header: "Payment Method",
+    cell: (cell) => (
+      <Chip
+        size="small"
+        label={cell.row.original.payment_method.toUpperCase()}
+        variant="filled"
+        color={
+          cell.row.original.payment_method === "wallet"
+            ? "primary"
+            : cell.row.original.payment_method === "CreditCard"
+            ? "success"
+            : cell.row.original.payment_method === "Benefit"
+            ? "info"
+            : cell.row.original.payment_method === "cash"
+            ? "error"
+            : "warning"
+        }
+      />
     ),
   },
 ];
