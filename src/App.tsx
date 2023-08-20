6c66c00763ea1128e73804d982c447ea6a5746ea
affan-habib <affan.habib44@gmail.com>
2023-08-20 12:11:19 +0600

    default home page is dashboard with a blank page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,21 @@ import LoginPage from "./features/auth/LoginPage";
 import ProductsPage from "./features/products/ProductsPage";
 import PrivateRoute from "./components/common/PrivateRoute";
 import SalesPage from "./features/sales/SalesPage";
+import Dashboard from "./features/dashboard/Dashboard";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
         <Route
           path="/products"
           element={
---
+++ src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { DashboardLayout } from "../../components/layouts/DashboardLayout";
+import { Container, Typography } from "@mui/material";
+
+const Dashboard: React.FC = () => {
+  return (
+    <DashboardLayout>
+      <Container
+        maxWidth="xl"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        <Typography variant="h1" component="h1">
+          Coming Soon
+        </Typography>
+      </Container>
+    </DashboardLayout>
+  );
+};
+
+export default Dashboard;
