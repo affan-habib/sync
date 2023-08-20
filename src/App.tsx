611b9bc1e957d8dfd9baddeaf8aa1c612f8bec9a
affan-habib <affan.habib44@gmail.com>
2023-08-20 11:45:46 +0600

    logout functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import LoginPage from './features/auth/LoginPage';
-import ProductsPage from './features/products/ProductsPage';
-import PrivateRoute from './components/common/PrivateRoute';
-import SalesPage from './features/sales/SalesPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./features/auth/LoginPage";
+import ProductsPage from "./features/products/ProductsPage";
+import PrivateRoute from "./components/common/PrivateRoute";
+import SalesPage from "./features/sales/SalesPage";
 
 const App: React.FC = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/products" element={<PrivateRoute><ProductsPage/></PrivateRoute>}/>
-                <Route path="/sales" element={<PrivateRoute><SalesPage/></PrivateRoute>}/>
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/products"
+          element={
+            <PrivateRoute>
+              <ProductsPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/sales"
+          element={
+            <PrivateRoute>
+              <SalesPage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
