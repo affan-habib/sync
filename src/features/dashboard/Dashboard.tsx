3c890d5ea702be96a431e4f546f0b3c428a94fbf
affan-habib <affan.habib44@gmail.com>
2023-08-22 15:28:15 +0600

    dashboard page updated

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { DashboardLayout } from "components/layouts/DashboardLayout";
-import { Container, Typography } from "@mui/material";
+import { Button, Container, Stack, Typography } from "@mui/material";
+import { items } from "components/layouts/config";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <DashboardLayout>
       <Container
@@ -14,9 +17,25 @@ const Dashboard: React.FC = () => {
           height: "100%",
         }}
       >
-        <Typography variant="h1" component="h1">
-          Coming Soon
-        </Typography>
+        <div>
+          <Typography variant="h1" component="h1">
+            Home page Coming soon
+          </Typography>
+          <Typography variant="subtitle1" component="h1" align="center" gutterBottom>
+            Currently you can visit below routes
+          </Typography>
+          <Stack direction="row" alignItems="center" justifyContent="center" spacing={2}>
+            {items.map((item) => (
+              <Button
+                variant="contained"
+                key={item.path}
+                onClick={() => navigate(item.path)}
+              >
+                {item.path}
+              </Button>
+            ))}
+          </Stack>
+        </div>
       </Container>
     </DashboardLayout>
   );
